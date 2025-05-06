9cb103ffac283ee4c071078d6a321cda16544d8e
OCEAN RAI <sagar.rai2023@outlook.com>
2025-05-06 16:37:41 -0400

    fix: removed unused header file

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
 #include <string>
-#include "asio.hpp"
-#include "crow.h" // Crow depends on ASIO library
-#include "../include/routes/AddContact.h"
+
 #include "../include/Contact.h"
 #include "../include/database/InsertContact.h"
-#include "crow/middlewares/cors.h" // CORS middleware to allow frontend access
+#include "../include/routes/AddContact.h"
 #include "../include/routes/DisplayContect.h"
+#include "asio.hpp"
+#include "crow.h"                   // Crow depends on ASIO library
+#include "crow/middlewares/cors.h"  // CORS middleware to allow frontend access
 
 // Alias for CORS middleware
 using CORS = crow::CORSHandler;
 
-
-
 int main(int argc, const char* argv[]) {
-
     // Create a Crow appp with CORS enabled
     crow::App<crow::CORSHandler> app;
 
     // Configure global CORS policy
-    app.get_middleware<crow::CORSHandler>() 
-    .global()
-    .origin("*") // Allow all origins
-    .methods("GET"_method, "POST"_method, "OPTIONS"_method)
-    .headers("Content-Type");
-    
+    app.get_middleware<crow::CORSHandler>()
+        .global()
+        .origin("*")  // Allow all origins
+        .methods("GET"_method, "POST"_method, "OPTIONS"_method)
+        .headers("Content-Type");
+
     AddContact(app);
     DisplayContact(app);
-    
 
- //************** Temporary code to Delete all data from MySQL Database ****************** */
+    //************** Temporary code to Delete all data from MySQL Database ****************** */
 
     // try {
     //     // Connects to database
-    //     auto connect = std::unique_ptr<sql::Connection>(getConnection()); 
-    //     // Prepare message to send 
+    //     auto connect = std::unique_ptr<sql::Connection>(getConnection());
+    //     // Prepare message to send
     //     std::unique_ptr<sql::Statement> stml(connect->createStatement());
     //     // DELETE FROM table_name;
     //     stml->executeQuery("DELETE FROM contacts");
@@ -43,11 +40,10 @@ int main(int argc, const char* argv[]) {
     // } catch (const std::exception& e) {
     //     std::cerr  << "[ERROR] Failed to delete contacts: " << e.what() << "\n";
     // }
-    
-     //************** Temporary code to Delete all data from MySQL Database ****************** */
 
+    //************** Temporary code to Delete all data from MySQL Database ****************** */
 
     app.port(8080).multithreaded().run();
-   
+
     return 0;
 }
